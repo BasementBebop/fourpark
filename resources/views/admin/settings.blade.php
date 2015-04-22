c5c09a3fd9fd033e0e5f3b3921b18ffc8edd0239
Christopher Joy-Webb <chris@joywebb.com>
2015-04-22 10:51:22 -0700

    add instructions portlet and clean up unnecessary code

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -1,93 +1,131 @@
 @extends('layouts.master')
 
 @section('content')
-
-	<div class="portlet box green col-md-6">
-		<div class="portlet-title">
-			<div class="caption">
-				Edit Your Info
+	
+	{{-- BEGIN EDIT SETTINGS PORTLET --}}
+	<div class="col-md-6">
+		<div class="portlet box green">
+			<div class="portlet-title">
+				<div class="caption">
+					Edit Your Info
+				</div>
+			</div>
+			<div class="portlet-body form">
+				<!-- BEGIN FORM-->
+				<form action="#" class="form-horizontal">
+					<div class="form-body">
+						<div class="form-group">
+							<label class="col-md-4 control-label">First Name</label>
+							<div class="col-md-8">
+								<input type="text" class="form-control" placeholder="Enter first name">
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-md-4 control-label">Last Name</label>
+							<div class="col-md-8">
+								<input type="text" class="form-control" placeholder="Enter last name">
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-md-4 control-label">Phone number</label>
+							<div class="col-md-8">
+								<input type="text" class="form-control" placeholder="Phone number">
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-md-4 control-label">Email Address</label>
+							<div class="col-md-8">
+								<div class="input-group">
+									<span class="input-group-addon">
+									<i class="fa fa-envelope"></i>
+									</span>
+									<input type="email" class="form-control" placeholder="Email Address">
+								</div>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-md-4 control-label">Old Password</label>
+							<div class="col-md-8">
+								<div class="input-group">
+									<input type="password" class="form-control" placeholder="Password">
+									<span class="input-group-addon">
+									<i class="fa fa-user"></i>
+									</span>
+								</div>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-md-4 control-label">New Password</label>
+							<div class="col-md-8">
+								<div class="input-group">
+									<input type="password" class="form-control" placeholder="Password">
+									<span class="input-group-addon">
+									<i class="fa fa-user"></i>
+									</span>
+								</div>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-md-4 control-label">Confirm Password</label>
+							<div class="col-md-8">
+								<div class="input-group">
+									<input type="password" class="form-control" placeholder="Password">
+									<span class="input-group-addon">
+									<i class="fa fa-user"></i>
+									</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="form-actions">
+						<div class="row">
+							<div class="col-md-offset-3 col-md-9">
+								<button type="submit" class="btn blue">Submit</button>
+								<button type="button" class="btn default">Cancel</button>
+							</div>
+						</div>
+					</div>
+				</form>
+				<!-- END FORM-->
 			</div>
 		</div>
-
-		<div class="portlet-body form">
-			<!-- BEGIN FORM-->
-			<form action="#" class="form-horizontal">
-				<div class="form-body">
-					<div class="form-group">
-						<label class="col-md-4 control-label">First Name</label>
-						<div class="col-md-8">
-							<input type="text" class="form-control" placeholder="Enter first name">
-						</div>
-					</div>
-					<div class="form-group">
-						<label class="col-md-4 control-label">Last Name</label>
-						<div class="col-md-8">
-							<input type="text" class="form-control" placeholder="Enter last name">
-						</div>
-					</div>
-					<div class="form-group">
-						<label class="col-md-4 control-label">Phone number</label>
-						<div class="col-md-8">
-							<input type="text" class="form-control" placeholder="Phone number">
-						</div>
-					</div>
-					<div class="form-group">
-						<label class="col-md-4 control-label">Email Address</label>
-						<div class="col-md-8">
-							<div class="input-group">
-								<span class="input-group-addon">
-								<i class="fa fa-envelope"></i>
-								</span>
-								<input type="email" class="form-control" placeholder="Email Address">
-							</div>
-						</div>
-					</div>
-					<div class="form-group">
-						<label class="col-md-4 control-label">Old Password</label>
-						<div class="col-md-8">
-							<div class="input-group">
-								<input type="password" class="form-control" placeholder="Password">
-								<span class="input-group-addon">
-								<i class="fa fa-user"></i>
-								</span>
-							</div>
-						</div>
-					</div>
-					<div class="form-group">
-						<label class="col-md-4 control-label">New Password</label>
-						<div class="col-md-8">
-							<div class="input-group">
-								<input type="password" class="form-control" placeholder="Password">
-								<span class="input-group-addon">
-								<i class="fa fa-user"></i>
-								</span>
-							</div>
-						</div>
-					</div>
-					<div class="form-group">
-						<label class="col-md-4 control-label">Confirm Password</label>
-						<div class="col-md-8">
-							<div class="input-group">
-								<input type="password" class="form-control" placeholder="Password">
-								<span class="input-group-addon">
-								<i class="fa fa-user"></i>
-								</span>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="form-actions">
-					<div class="row">
-						<div class="col-md-offset-3 col-md-9">
-							<button type="submit" class="btn blue">Submit</button>
-							<button type="button" class="btn default">Cancel</button>
-						</div>
-					</div>
-				</div>
-			</form>
-			<!-- END FORM-->
-		</div>
-
 	</div>
-
+	{{-- END EDIT SETTINGS PORTLET --}}
+	{{-- BEGIN INSTRUCTIONS PORTLET --}}
+	<div class="col-md-6">
+		<div class="portlet solid blue">
+			<div class="portlet-title">
+				<div class="caption">
+					FourPark Instructions
+				</div>
+			</div>
+			<div class="portlet-body">
+				<div class="scroller" style="height:100%" data-rail-visible="1" data-rail-color="yellow" data-handle-color="#a1b2bd">
+					<p class="bold">Parking spots will be assigned randomly.</p>
+					<p class="bold">I have a parking spot:</p>
+					<ul>
+						<li>You can give up your spot until 5pm the night before. After 5pm, no changes can be made until the next morning.</li>
+						<li>You can reclaim your spot if it has not been assigned to another employee.</li>
+					</ul>
+					<p class="bold">I want a parking spot:</p>
+					<ul>
+						<li>If you are assigned a spot, you have:
+							<ul>
+								<li>
+									30 minutes to accept at night
+								</li>
+								<li>
+									10 minutes to accept in the morning
+								</li>
+							</ul>
+						</li>
+						<li>
+							You can accept the spot, deny the spot, or do nothing.
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	{{-- END INSTRUCTIONS PORTLET --}}
 @stop
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -21,222 +21,210 @@
 	<link href="../../assets/global/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css"/>
 	<link href="../../assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css"/>
 	<!-- END GLOBAL MANDATORY STYLES -->
-	
-	<!-- BEGIN PAGE LEVEL STYLES -->
-	<link rel="stylesheet" type="text/css" href="../../assets/global/plugins/select2/select2.css"/>
-	<link rel="stylesheet" type="text/css" href="../../assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.css"/>
-	<!-- END PAGE LEVEL STYLES -->
-
 	<!-- BEGIN THEME STYLES -->
 	<link href="../../assets/global/css/components.css" id="style_components" rel="stylesheet" type="text/css"/>
 	<link href="../../assets/global/css/plugins.css" rel="stylesheet" type="text/css"/>
 	<link href="../../assets/admin/layout/css/layout.css" rel="stylesheet" type="text/css"/>
 	<link id="style_color" href="../../assets/admin/layout/css/themes/darkblue.css" rel="stylesheet" type="text/css"/>
 	<link href="../../assets/admin/layout/css/custom.css" rel="stylesheet" type="text/css"/>
+	<link rel="stylesheet" type="text/css" href="../../assets/global/plugins/select2/select2.css"/>
+	<link rel="stylesheet" type="text/css" href="../../assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.css"/>
 	<!-- END THEME STYLES -->
+
+	{{-- BEGIN CUSTOM STYLES --}}
+	<link rel="stylesheet" type="text/css" href="../../assets/custom/styles.css">
+	{{-- END CUSTOM STYLES --}}
+
 	<link rel="shortcut icon" href="favicon.ico"/>
 </head>
 <!-- END HEAD -->
 <!-- BEGIN BODY -->
 <body class="page-header-fixed page-quick-sidebar-over-content ">
-<!-- BEGIN HEADER -->
-<div class="page-header navbar navbar-fixed-top">
-	<!-- BEGIN HEADER INNER -->
-	<div class="page-header-inner">
-		<!-- BEGIN LOGO -->
-		<div class="page-logo">
-			<a href="/">
-			<img src="../../assets/admin/layout/img/logo.png" alt="logo" class="logo-default"/>
+	<!-- BEGIN HEADER -->
+	<div class="page-header navbar navbar-fixed-top">
+		<!-- BEGIN HEADER INNER -->
+		<div class="page-header-inner">
+			<!-- BEGIN LOGO -->
+			<div class="page-logo">
+				<a href="/">
+				<img src="../../assets/admin/layout/img/logo.png" alt="logo" class="logo-default"/>
+				</a>
+			</div>
+			<!-- END LOGO -->
+			<!-- BEGIN RESPONSIVE MENU TOGGLER -->
+			<a href="javascript:;" class="menu-toggler responsive-toggler" data-toggle="collapse" data-target=".navbar-collapse">
 			</a>
+			<!-- END RESPONSIVE MENU TOGGLER -->
+			<!-- BEGIN TOP NAVIGATION MENU -->
+			<div class="top-menu">
+				<ul class="nav navbar-nav pull-right">
+					<!-- BEGIN USER LOGIN DROPDOWN -->
+					<li class="dropdown dropdown-user">
+						<a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
+						<span class="username">
+						Chris </span>
+						<i class="fa fa-angle-down"></i>
+						</a>
+						<ul class="dropdown-menu dropdown-menu-default">
+							<li>
+								<a href="settings">
+								<i class="icon-user"></i> Settings </a>
+							</li>
+							<li class="divider">
+							</li>
+							<li>
+								<a href="login">
+								<i class="icon-key"></i> Log Out </a>
+							</li>
+						</ul>
+					</li>
+					<!-- END USER LOGIN DROPDOWN -->
+				</ul>
+			</div>
+			<!-- END TOP NAVIGATION MENU -->
 		</div>
-		<!-- END LOGO -->
-		<!-- BEGIN RESPONSIVE MENU TOGGLER -->
-		<a href="javascript:;" class="menu-toggler responsive-toggler" data-toggle="collapse" data-target=".navbar-collapse">
-		</a>
-		<!-- END RESPONSIVE MENU TOGGLER -->
-		<!-- BEGIN TOP NAVIGATION MENU -->
-		<div class="top-menu">
-			<ul class="nav navbar-nav pull-right">
-				<!-- BEGIN USER LOGIN DROPDOWN -->
-				<li class="dropdown dropdown-user">
-					<a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
-					<span class="username">
-					Chris </span>
-					<i class="fa fa-angle-down"></i>
-					</a>
-					<ul class="dropdown-menu dropdown-menu-default">
-						<li>
-							<a href="settings">
-							<i class="icon-user"></i> Settings </a>
-						</li>
-						<li class="divider">
-						</li>
-						<li>
-							<a href="login">
-							<i class="icon-key"></i> Log Out </a>
-						</li>
-					</ul>
-				</li>
-				<!-- END USER LOGIN DROPDOWN -->
-			</ul>
-		</div>
-		<!-- END TOP NAVIGATION MENU -->
+		<!-- END HEADER INNER -->
 	</div>
-	<!-- END HEADER INNER -->
-</div>
-<!-- END HEADER -->
-<div class="clearfix">
-</div>
-<!-- BEGIN CONTAINER -->
-<div class="page-container">
-	<!-- BEGIN SIDEBAR -->
-	<div class="page-sidebar-wrapper">
-		<div class="page-sidebar navbar-collapse collapse">
-			<!-- BEGIN SIDEBAR MENU -->
-			<ul class="page-sidebar-menu page-sidebar-menu-light page-sidebar-menu-hover-submenu" data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200">
-				<li class="sidebar-toggler-wrapper">
-					<!-- BEGIN SIDEBAR TOGGLER BUTTON -->
-					<div class="sidebar-toggler">
-					</div>
-					<!-- END SIDEBAR TOGGLER BUTTON -->
-				</li>
-				<li class="start">
-					<a href="dashboard">
-					<i class="icon-home"></i>
-					<span class="title">Dashboard</span>
-					</a>
-				</li>
-				<li class="start">
-					<a href="users">
-					<i class="icon-users"></i>
-					<span class="title">Users</span>
-					</a>
-				</li>
-				<li class="start">
-					<a href="settings">
-					<i class="icon-settings"></i>
-					<span class="title">Settings</span>
-					</a>
-				</li>
-			<!-- END SIDEBAR MENU -->
-		</div>
+	<!-- END HEADER -->
+	<div class="clearfix">
 	</div>
-	<!-- END SIDEBAR -->
-
-
-
-	<!-- BEGIN CONTENT -->
-	<div class="page-content-wrapper">
-		<div class="page-content">
-			<!-- BEGIN PAGE HEADER-->
-			<h3 class="page-title">
-
-				@yield('title')
-
-			</h3>
-			<!-- END PAGE HEADER-->
-			<div class="row">
-				<div class="col-md-12 col-sm-12">
-
-					@yield('content')
-
-				</div>
+	<!-- BEGIN CONTAINER -->
+	<div class="page-container">
+		<!-- BEGIN SIDEBAR -->
+		<div class="page-sidebar-wrapper">
+			<div class="page-sidebar navbar-collapse collapse">
+				<!-- BEGIN SIDEBAR MENU -->
+				<ul class="page-sidebar-menu page-sidebar-menu-light page-sidebar-menu-hover-submenu" data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200">
+					<li class="sidebar-toggler-wrapper">
+						<!-- BEGIN SIDEBAR TOGGLER BUTTON -->
+						<div class="sidebar-toggler">
+						</div>
+						<!-- END SIDEBAR TOGGLER BUTTON -->
+					</li>
+					<li class="start">
+						<a href="dashboard">
+						<i class="icon-home"></i>
+						<span class="title">Dashboard</span>
+						</a>
+					</li>
+					<li class="start">
+						<a href="users">
+						<i class="icon-users"></i>
+						<span class="title">Users (Admin only)</span>
+						</a>
+					</li>
+					<li class="start">
+						<a href="settings">
+						<i class="icon-settings"></i>
+						<span class="title">Settings</span>
+						</a>
+					</li>
+				<!-- END SIDEBAR MENU -->
 			</div>
 		</div>
+		<!-- END SIDEBAR -->
+		<!-- BEGIN CONTENT -->
+		<div class="page-content-wrapper">
+			<div class="page-content">
+				<!-- BEGIN PAGE HEADER-->
+				<h3 class="page-title">
+
+					@yield('title')
+
+				</h3>
+				<!-- END PAGE HEADER-->
+
+				{{-- BEGIN MAIN CONTENT --}}
+				<div class="row">
+					<div class="col-md-12 col-sm-12">
+
+						@yield('content')
+
+					</div>
+				</div>
+				{{-- END MAIN CONTENT --}}
+			</div>
+		</div>
+		<!-- END CONTENT -->
 	</div>
-	<!-- END CONTENT -->
-</div>
-<!-- END CONTAINER -->
-<!-- BEGIN FOOTER -->
-<div class="page-footer">
-	<div class="page-footer-inner">
-		 2015 &copy; FourPark by Adpearance
+	<!-- END CONTAINER -->
+	<!-- BEGIN FOOTER -->
+	<div class="page-footer">
+		<div class="page-footer-inner">
+			 2015 &copy; FourPark by Adpearance
+		</div>
+		<div class="scroll-to-top">
+			<i class="icon-arrow-up"></i>
+		</div>
 	</div>
-	<div class="scroll-to-top">
-		<i class="icon-arrow-up"></i>
-	</div>
-</div>
-<!-- END FOOTER -->
-<!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
-<!-- BEGIN CORE PLUGINS -->
-<!--[if lt IE 9]>
-<script src="../../assets/global/plugins/respond.min.js"></script>
-<script src="../../assets/global/plugins/excanvas.min.js"></script>
-<![endif]-->
-<script src="../../assets/global/plugins/jquery.min.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/jquery-migrate.min.js" type="text/javascript"></script>
-<!-- IMPORTANT! Load jquery-ui.min.js before bootstrap.min.js to fix bootstrap tooltip conflict with jquery ui tooltip -->
-<script src="../../assets/global/plugins/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/jquery.cokie.min.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/uniform/jquery.uniform.min.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
-<!-- END CORE PLUGINS -->
-<!-- BEGIN PAGE LEVEL PLUGINS -->
+	<!-- END FOOTER -->
+	<!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
+	<!-- BEGIN CORE PLUGINS -->
+	<!--[if lt IE 9]>
+	<script src="../../assets/global/plugins/respond.min.js"></script>
+	<script src="../../assets/global/plugins/excanvas.min.js"></script>
+	<![endif]-->
+	<script src="../../assets/global/plugins/jquery.min.js" type="text/javascript"></script>
+	<script src="../../assets/global/plugins/jquery-migrate.min.js" type="text/javascript"></script>
+	<!-- IMPORTANT! Load jquery-ui.min.js before bootstrap.min.js to fix bootstrap tooltip conflict with jquery ui tooltip -->
+	<script src="../../assets/global/plugins/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
+	<script src="../../assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="../../assets/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
+	<script src="../../assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+	<script src="../../assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
+	<script src="../../assets/global/plugins/jquery.cokie.min.js" type="text/javascript"></script>
+	<script src="../../assets/global/plugins/uniform/jquery.uniform.min.js" type="text/javascript"></script>
+	<script src="../../assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
+	<!-- END CORE PLUGINS -->
+	<!-- BEGIN PAGE LEVEL PLUGINS -->
+	<script type="text/javascript" src="../../assets/global/plugins/select2/select2.min.js"></script>
+	<script type="text/javascript" src="../../assets/global/plugins/datatables/media/js/jquery.dataTables.min.js"></script>
+	<script type="text/javascript" src="../../assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.js"></script>
+	<script src="../../assets/global/plugins/flot/jquery.flot.min.js" type="text/javascript"></script>
+	<script src="../../assets/global/plugins/flot/jquery.flot.resize.min.js" type="text/javascript"></script>
+	<script src="../../assets/global/plugins/flot/jquery.flot.categories.min.js" type="text/javascript"></script>
+	<script src="../../assets/global/plugins/jquery.pulsate.min.js" type="text/javascript"></script>
+	<script src="../../assets/global/plugins/bootstrap-daterangepicker/moment.min.js" type="text/javascript"></script>
+	<script src="../../assets/global/plugins/bootstrap-daterangepicker/daterangepicker.js" type="text/javascript"></script>
+	<!-- IMPORTANT! fullcalendar depends on jquery-ui.min.js for drag & drop support -->
+	<script src="../../assets/global/plugins/fullcalendar/fullcalendar.min.js" type="text/javascript"></script>
+	<script src="../../assets/global/plugins/jquery-easypiechart/jquery.easypiechart.min.js" type="text/javascript"></script>
+	<script src="../../assets/global/plugins/jquery.sparkline.min.js" type="text/javascript"></script>
+	<!-- END PAGE LEVEL PLUGINS -->
+	<!-- BEGIN PAGE LEVEL SCRIPTS -->
+	<script src="../../assets/global/scripts/metronic.js" type="text/javascript"></script>
+	<script src="../../assets/admin/layout/scripts/layout.js" type="text/javascript"></script>
+	<script src="../../assets/admin/pages/scripts/index.js" type="text/javascript"></script>
+	<script src="../../assets/admin/pages/scripts/tasks.js" type="text/javascript"></script>
+	<script src="../../assets/admin/pages/scripts/table-editable.js"  type="text/javascript"></script>
+	<!-- END PAGE LEVEL SCRIPTS -->
 
-<script type="text/javascript" src="../../assets/global/plugins/select2/select2.min.js"></script>
-<script type="text/javascript" src="../../assets/global/plugins/datatables/media/js/jquery.dataTables.min.js"></script>
-<script type="text/javascript" src="../../assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.js"></script>
+	{{-- BEGIN CUSTOM SCRIPTS --}}
+	<script src="../../assets/custom/scripts.js" type="text/javascript"></script>
+	{{-- END CUSTOM SCRIPTS --}}
 
-
-
-<script src="../../assets/global/plugins/jqvmap/jqvmap/jquery.vmap.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.russia.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.world.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.europe.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.germany.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.usa.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/jqvmap/jqvmap/data/jquery.vmap.sampledata.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/flot/jquery.flot.min.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/flot/jquery.flot.resize.min.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/flot/jquery.flot.categories.min.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/jquery.pulsate.min.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/bootstrap-daterangepicker/moment.min.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/bootstrap-daterangepicker/daterangepicker.js" type="text/javascript"></script>
-<!-- IMPORTANT! fullcalendar depends on jquery-ui.min.js for drag & drop support -->
-<script src="../../assets/global/plugins/fullcalendar/fullcalendar.min.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/jquery-easypiechart/jquery.easypiechart.min.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/jquery.sparkline.min.js" type="text/javascript"></script>
-<!-- END PAGE LEVEL PLUGINS -->
-<!-- BEGIN PAGE LEVEL SCRIPTS -->
-<script src="../../assets/global/scripts/metronic.js" type="text/javascript"></script>
-<script src="../../assets/admin/layout/scripts/layout.js" type="text/javascript"></script>
-<script src="../../assets/admin/layout/scripts/quick-sidebar.js" type="text/javascript"></script>
-<script src="../../assets/admin/layout/scripts/demo.js" type="text/javascript"></script>
-<script src="../../assets/admin/pages/scripts/index.js" type="text/javascript"></script>
-<script src="../../assets/admin/pages/scripts/tasks.js" type="text/javascript"></script>
-<script src="../../assets/admin/pages/scripts/table-editable.js"></script>
-<!-- END PAGE LEVEL SCRIPTS -->
-
-<script>
-jQuery(document).ready(function() {
-   Metronic.init(); // init metronic core componets
-   Layout.init(); // init layout
-   QuickSidebar.init(); // init quick sidebar
-   Index.init();
-   Index.initDashboardDaterange();
-   Index.initJQVMAP(); // init index page's custom scripts
-   Index.initCalendar(); // init index page's custom scripts
-   Index.initCharts(); // init index page's custom scripts
-   Index.initChat();
-   Index.initMiniCharts();
-   Tasks.initDashboardWidget();
-   TableEditable.init();
-});
-</script>
-<!-- END JAVASCRIPTS -->
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-37564768-1', 'keenthemes.com');
-  ga('send', 'pageview');
-</script>
+	<script>
+	jQuery(document).ready(function() {
+	   Metronic.init(); // init metronic core componets
+	   Layout.init(); // init layout
+	   Index.init();
+	   Index.initDashboardDaterange();
+	   Index.initCalendar(); // init index page's custom scripts
+	   Index.initCharts(); // init index page's custom scripts
+	   Index.initMiniCharts();
+	   Tasks.initDashboardWidget();
+	   TableEditable.init();
+	});
+	</script>
+	<!-- END JAVASCRIPTS -->
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	  ga('create', 'UA-37564768-1', 'keenthemes.com');
+	  ga('send', 'pageview');
+	</script>
 </body>
-
 <!-- END BODY -->
 </html>
